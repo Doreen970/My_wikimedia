6cd2fa510863bc9b07588f505d5927fa4ebca0ea
Doreen970 <doreenwanyama20@gmail.com>
2023-10-10 21:31:28 +0300

    This is the second task in my contribution. I was asked to create a model named Bug and to include the fields provided. I was then supposed to make an update to the model created on the admin panel. To achieve this I first of all included my app on the settings.py file then I created a superuser to manage the admin site. I had to run migrations also to make sure changes to the database were made

--- wiki_project/bug/models.py
+++ wiki_project/bug/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Bug(models.Model):
+    description = models.TextField()
+    bug_type = models.CharField(max_length=100)
+    report_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=[
+        ('To Do', 'To Do'),
+        ('In Progress', 'In Progress'),
+        ('Done', 'Done'),
+    ])
+
+    def __str__(self):
+        return self.bug_type
